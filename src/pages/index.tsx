e4e672626d78fea6ea9d1da8f068b8ce387946b9
Carlos Figueiredo <cvieirasp@gmail.com>
2021-05-28 14:47:01 -0300

    implementing authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { getSession, GetSessionOptions } from 'next-auth/client'
+import { Session } from 'next-auth'
 import Head from 'next/head'
+import Login from '../components/Login'
 import Header from '../components/Header'
 
-export default function Home() {
+interface Props {
+  session: Session
+}
+
+export default function Home({ session }: Props) {
+  if (!session) return <Login />
+
   return (
     <div>
       <Head>
@@ -11,4 +20,14 @@ export default function Home() {
       <Header />
     </div>
   )
+}
+
+export async function getServerSideProps(context: GetSessionOptions) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
